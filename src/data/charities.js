808816c90a4a2bbc63391680ac4b0e25c8658538
andrianoalamsyah09 <andrianoalamsyah09@gmail.com>
2022-03-05 14:40:12 +0700

    Convert HTML to svelte componets

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'Frist Charity Project',
+            category: 'money',
+            thumbnail: 'https://img.idxchannel.com/media/700/images/idx/2021/01/27/uang.jpg',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('10 August 2022'),
+            profile_foto: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366__340.png',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 99000
+
+        }
+
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,22 @@
+<script>
+	import CharityList from '../components/CharityList.svelte' ;
+	import Header from '../components/Header.svelte' ;
+	import Welcome from '../components/Welcome.svelte';
+	import Promo from '../components/Promo.svelte' ;
+	import Footer from '../components/Footer.svelte' ;
+
+	import  { charities } from '../data/charities.js' ;
+
+	let title = "Charity";
+</script>
+ 
+
+ 
+<CharityList charities={ charities } />
+<Header />
+<Welcome />
+<Promo />
+<Footer/>
+
+
+
