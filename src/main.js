e5f32651965b8ae8444ebf8d8902269d5837aa39
andrianoalamsyah09 <andrianoalamsyah09@gmail.com>
2022-03-08 11:16:03 +0700

    Convert HTML to svelte components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import App from './App.svelte';
 
 const app = new App({
-<<<<<<< HEAD
 	target: document.querySelector("#root")
-=======
-	target: document.querySelector("#root"),
->>>>>>> 808816c90a4a2bbc63391680ac4b0e25c8658538
+
 
 });
 
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,4 @@
 <script>
-<<<<<<< HEAD
 	import CharityList from "../componets/CharityList.svelte";
 	import Header from "../componets/Header.svelte";
 	import Welcome from "../componets/Welcome.svelte";
@@ -17,26 +16,3 @@
 <CharityList  { charities } />
 <Promo />
 <Footer />
-=======
-	import CharityList from '../components/CharityList.svelte' ;
-	import Header from '../components/Header.svelte' ;
-	import Welcome from '../components/Welcome.svelte';
-	import Promo from '../components/Promo.svelte' ;
-	import Footer from '../components/Footer.svelte' ;
-
-	import  { charities } from '../data/charities.js' ;
-
-	let title = "Charity";
-</script>
- 
-
- 
-<CharityList charities={ charities } />
-<Header />
-<Welcome />
-<Promo />
-<Footer/>
-
-
-
->>>>>>> 808816c90a4a2bbc63391680ac4b0e25c8658538
