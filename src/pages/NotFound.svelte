73dcf7263b44790dc3bc105991a5b5a1013c5c00
andrianoalamsyah09 <andrianoalamsyah09@gmail.com>
2022-03-11 10:58:57 +0700

    Add NotFound config file

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -3,11 +3,11 @@
     <div class="container">
         <div class="xs-heading row xs-mb-60 justify-content-center text-center">
             <div class="col-md-6 col-xl-6">
-                <img src="/assets/images/donation_error.png" alt="" width="450" height="450">
+                <img src="/assets/images/notfound.png" alt="" width="450" height="450">
                 <h2 class="xs-title">Oops!</h2>
-                <p>There is something wrong with the payment please try again or cancel the donation.</p>
+                <p>ngapurane mas/mbak njenengan salah ketik</p>
                 <a href="/" class="btn btn-primary mt-4">
-                    Try Again
+                    monggo dicobo maleh
                 </a>
             </div><!-- .xs-heading-title END -->
         </div><!-- .row end -->
